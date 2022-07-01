aaa1a83e79e22f88ac5869c99a9d6a7d03977469
linhlc <conglinhcse@gmail.com>
2022-07-01 22:39:05 +0700

    update reg

--- regression/dataset.py
+++ regression/dataset.py
@@ -2,19 +2,29 @@ import os
 import math
 
 import numpy as np
-import matplotlib.pyplot as plt
 from PIL import Image
 import torch
 from torchvision import datasets, models, transforms
 import torch.nn as nn
-from torch.nn import functional as F
-import torch.optim as optim
+from torch.utils.data import DataLoader
 
 
 class FeedLaneDataset():
     def __init__(self) -> None:
         # Create PyTorch data generators
         self.normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])    
+        label_dict_raw = {
+            "empty": 0,
+            "minimal": 1,
+            "normal": 2,
+            "full": 3,
+        }
+        label_dict = {
+            0:0,
+            1:3,
+            2:1,
+            3:2,
+        }
         data_transforms = {
             'train':
             transforms.Compose([
@@ -34,9 +44,9 @@ class FeedLaneDataset():
 
         self.image_datasets = {
             'train': 
-            datasets.ImageFolder('data/classified_data/train', data_transforms['train']),
+            datasets.ImageFolder('data/classified_data/train', data_transforms['train'], target_transform=lambda x: label_dict[x]),
             'val': 
-            datasets.ImageFolder('data/classified_data/val', data_transforms['val'])
+            datasets.ImageFolder('data/classified_data/val', data_transforms['val'], target_transform=lambda x: label_dict[x])
         }
     
     def get_len(self, data_type):
@@ -44,13 +54,11 @@ class FeedLaneDataset():
 
     def data_loader(self):
         dataloaders = {
-        'train':
-        torch.utils.data.DataLoader(self.image_datasets['train'],
+        'train':DataLoader(self.image_datasets['train'],
                                     batch_size=32,
                                     shuffle=True, num_workers=4),
-        'val':
-        torch.utils.data.DataLoader(self.image_datasets['val'],
-                                    batch_size=32,
+        'val':DataLoader(self.image_datasets['val'],
+                                    batch_size=8,
                                     shuffle=False, num_workers=4)
         }
 
--- regression/model.py
+++ regression/model.py
@@ -9,113 +9,137 @@ from torchvision import datasets, models, transforms
 import torch.nn as nn
 from torch.nn import functional as F
 import torch.optim as optim
+import torchmetrics
+from pylab import savefig
+import seaborn as sn
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
 
-from .dataset import FeedLaneDataset
+
+from dataset import FeedLaneDataset
 
 class FeedLaneModel():
     def __init__(self):
         # Create the network
         self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-        self.model = models.resnet50(pretrained=False).to(self.device)
+        # self.device = "cpu"
+        self.model = models.resnet18(pretrained=True).to(self.device)
             
-        for param in self.model.parameters():
-            param.requires_grad = False   
+        # for param in self.model.parameters():
+        #     param.requires_grad = False   
 
         self.model.fc = nn.Sequential(
-                    nn.Linear(2048, 128),
-                    nn.ReLU(inplace=True),
-                    nn.Linear(128, 64),
+                    # nn.Linear(512, 128),
+                    # nn.ReLU(inplace=True),
+                    nn.Linear(512, 64),
                     nn.ReLU(inplace=True),
                     nn.Linear(64, 1)).to(self.device)
         self.criterion = nn.MSELoss()
-        self.optimizer = optim.Adam(self.model.fc.parameters())
-  
+        self.optimizer = optim.Adam(self.model.parameters())
+
+        os.makedirs("savemodel/pytorch", exist_ok=True)
+        self.ckpt_path = 'savemodel/pytorch/weights.pth'
+
+    def count_correct(self, preds, labels, epsilon):
+        running_corrects = 0
+        for idx, value in enumerate(labels):
+            if value == 0.0 and preds[idx] < value + epsilon:
+                running_corrects += 1
+            elif value == 0.1 and preds[idx] > value - epsilon and preds[idx] < value + epsilon:
+                running_corrects += 1
+            elif value == 0.2 and preds[idx] > value - epsilon and preds[idx] < value + epsilon:
+                running_corrects += 1
+            elif value == 0.3 and preds[idx] > value - epsilon :
+                running_corrects += 1
+        return running_corrects
+
     # Train the model
-    def train_model(self, dataset, num_epochs=100):
+    def train_model(self, dataset, num_epochs=100, epsilon=0.05):
+        best_loss = 1000
         dataloader = dataset.data_loader()
         for epoch in range(num_epochs):
             print('Epoch {}/{}'.format(epoch+1, num_epochs))
-            print('-' * 50)
+            print('-' * 100)
 
-            for phase in ['train', 'val']:
+            for phase in ['train']:
                 if phase == 'train':
                     self.model.train()
-                else:
-                    self.model.eval()
+                # else:
+                #     self.model.eval()
 
-                running_loss = 0.0
-                running_corrects = 0
+                running_outputs = torch.tensor([])
+                running_labels = torch.tensor([])
 
                 for inputs, labels in dataloader[phase]:
                     inputs = inputs.to(self.device)
-                    labels = labels.type(torch.float32)
+                    labels = labels.type(torch.float32) / 10
                     labels = labels.to(self.device)
 
-                    outputs = torch.squeeze(self.model(inputs))
-                    loss = self.criterion(outputs, labels)
+                    outputs = torch.squeeze(self.model(inputs))              
 
                     if phase == 'train':
+                        loss = self.criterion(outputs, labels)
                         self.optimizer.zero_grad()
                         loss.backward()
                         self.optimizer.step()
 
-                    preds = outputs
-                    running_loss += loss.detach()
-                    running_corrects += torch.sum(torch.round(preds) == labels.data)
-                    # if phase == 'val':
-                    #     print('{} loss: {:.4f}, acc: {:.4f}'.format(phase,running_loss.item(),running_corrects.item()))
+                    running_outputs = torch.cat((running_outputs,outputs.cpu()))
+                    running_labels = torch.cat((running_labels,labels.cpu()))
 
 
-                epoch_loss = running_loss / math.ceil(len(dataset.get_len(phase)) / 32)
-                epoch_acc = running_corrects.float() / len(dataset.get_len(phase))
-                
-                print('{} loss: {:.4f}, acc: {:.4f}'.format(phase,epoch_loss.item(),epoch_acc.item()))
+                epoch_loss = self.criterion(running_outputs, running_labels)
+                epoch_acc = self.count_correct(running_outputs, running_labels, epsilon) / len(running_outputs)
+                print('{} loss: {:.4f}, acc: {:.4f}'.format(phase,epoch_loss,epoch_acc))
+
+            if epoch_loss < best_loss: 
+                best_loss = epoch_loss
+                # torch.save(self.model, 'best-model.pt')
+                print(f"Saving best model at {self.ckpt_path} with loss {best_loss}")
+                torch.save(self.model.state_dict(), self.ckpt_path)
+               
         return self.model
 
     def train(self, dataset, num_epochs):
         model_trained = self.train_model(dataset, num_epochs=num_epochs)
-        #Save model
-        torch.save(model_trained.state_dict(), 'savemodel/pytorch/weights.pth')
-
-    def validate(self, dataset):
-        self.model.load_state_dict(torch.load('savemodel/pytorch/weights.pth'))
-        validation_img_paths = "/content/feedlane/data/classified_data/val"
-        # ls_file = []
-        # for f in os.listdir(validation_img_paths)[:20]:
-        #     ls_file.append(os.path.join(validation_img_paths,f))
-        # for i in range(0, len(ls_file), 10):
-        #     img_list = [Image.open(img_path) for img_path in ls_file[i:i+10]]
-        #     validation_batch = torch.stack([data_transforms['val'](img).to(device)
-        #                                 for img in img_list])
-        #     pred_logits_tensor = model(validation_batch)
-        #     print(pred_logits_tensor)
+        
+    
+    def validate(self, dataset, epsilon=0.05):
+        self.model.load_state_dict(torch.load(self.ckpt_path))
 
         self.model.eval()
-        val_loader = dataset.data_loader("val")
+        for param in self.model.parameters():
+            param.requires_grad = False
 
-        running_loss = 0
-        running_corrects = 0
+        val_loader = dataset.data_loader()["val"]
+
+        running_outputs = torch.tensor([])
+        running_labels = torch.tensor([])
+
         for inputs, labels in val_loader:
             inputs = inputs.to(self.device)
-            labels = labels.type(torch.float32)
-            # labels = torch.tensor(labels.clone().detach(), dtype=torch.float32)
+            labels = labels.type(torch.float32) / 10
             labels = labels.to(self.device)
-            # print(labels.data)
 
             outputs = torch.squeeze(self.model(inputs))
-            loss = self.criterion(outputs, labels)
-            preds = outputs
-            # import pdb; pdb.set_trace()
-            running_loss += loss.detach()
-            running_corrects += torch.sum(torch.round(preds) == labels.data)
 
-        epoch_loss = running_loss / math.ceil(len(val_loader) / 32)
-        epoch_acc = running_corrects.float() / len(val_loader)
+            running_outputs = torch.cat((running_outputs,outputs.cpu()))
+            running_labels = torch.cat((running_labels,labels.cpu()))
 
-        print('Validation loss: {:.4f}, acc: {:.4f}'.format(epoch_loss.item(), epoch_acc.item()))
+        loss = self.criterion(running_outputs, running_labels)
+        acc = self.count_correct(running_outputs, running_labels, epsilon) / len(running_outputs)
+
+        print('Validation loss: {:.4f}, acc: {:.4f}'.format(loss, acc))
+
+        # confusion matrix
+        num_classes = 4
+        cm = confusion_matrix((running_labels*10).type(torch.int32), torch.round(running_outputs*10), labels=[i for i in range(num_classes)])
+        disp = ConfusionMatrixDisplay(confusion_matrix=cm)
+        disp.plot()
+        plt.savefig('/content/feedlane/savemodel/pytorch/cm_reg.png')
     
 
 if __name__=="__main__":
     dataset = FeedLaneDataset()
-    model = FeedLaneModel(dataset, 100)
-    model.train()
+    model = FeedLaneModel()
+    # model.train(dataset, 100)
+    model.validate(dataset)
+    
